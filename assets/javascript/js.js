0ba6189bc9b14b12242f4ea8d9a1b885e27d3668
eric hoppenworth <eric.hoppenworth@gmail.com>
2017-06-17 09:00:17 -0400

    attempt at auth

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -9,46 +9,35 @@ var config = {
 };
 firebase.initializeApp(config);
 
-//authentication
+// Using a redirect.
+firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a GitHub Access Token.
+    var token = result.credential.accessToken;
+  }
+  var user = result.user;
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  if (errorCode === 'auth/account-exists-with-different-credential') {
+    alert('You have signed up with a different provider for that email.');
+    // Handle linking here if your app allows it.
+  } else {
+    console.error(error);
+  }
+});
+
+// Start a sign in process for an unauthenticated user.
 var provider = new firebase.auth.GithubAuthProvider();
+provider.addScope('repo');
+firebase.auth().signInWithRedirect(provider);
 
-// firebase.auth().signInWithRedirect(provider)
 
-// firebase.auth().getRedirectResult().then(function(result) {
-//   if (result.credential) {
-//     // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-//     var token = result.credential.accessToken;
-//     // ...
-//   }
-//   // The signed-in user info.
-//   var user = result.user;
-// }).catch(function(error) {
-//   // Handle Errors here.
-//   var errorCode = error.code;
-//   var errorMessage = error.message;
-//   // The email of the user's account used.
-//   var email = error.email;
-//   // The firebase.auth.AuthCredential type that was used.
-//   var credential = error.credential;
-//   // ...
-// });
-
-// firebase.auth().signInWithPopup(provider).then(function(result) {
-//   // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-//   var token = result.credential.accessToken;
-//   // The signed-in user info.
-//   var user = result.user;
-//   // ...
-// }).catch(function(error) {
-//   // Handle Errors here.
-//   var errorCode = error.code;
-//   var errorMessage = error.message;
-//   // The email of the user's account used.
-//   var email = error.email;
-//   // The firebase.auth.AuthCredential type that was used.
-//   var credential = error.credential;
-//   // ...
-// });
 
 //train object constructor for creating new train objects
 function Train(name, dest, arrival, freq){
